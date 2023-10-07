fc8f3bc2e83ade2fdb1170b827b8befaaf1c7074
acgc99 <acgc99@outlook.es>
2023-10-07 19:47:35 +0200

    Rework: WIconButton, WNavBar

--- addons/widgets/scripts/w_icon_button.gd
+++ addons/widgets/scripts/w_icon_button.gd
@@ -1,12 +1,46 @@
 @tool
 class_name WIconButton
-extends Button
+extends BaseButton
 ## Button that consists of an icon.
 
 
-func _enter_tree() -> void:
-	flat = true
-	icon_alignment = HORIZONTAL_ALIGNMENT_CENTER
-	expand_icon = true
-	anchors_preset = PRESET_FULL_RECT
-	focus_mode = Control.FOCUS_NONE
+## Icon texture.
+@export var icon: Texture2D:
+	set(icon_):
+		icon = icon_
+		if texture_rect_icon != null:
+			texture_rect_icon.texture = icon
+## If [code]true[/code], texture is flipped horizontally.
+@export var flip_h: bool:
+	set(flip_h_):
+		flip_h = flip_h_
+		if texture_rect_icon != null:
+			texture_rect_icon.flip_h = flip_h
+## If [code]true[/code], texture is flipped vertically.
+@export var flip_v: bool:
+	set(flip_v_):
+		flip_v = flip_v_
+		if texture_rect_icon != null:
+			texture_rect_icon.flip_v = flip_v
+## [TextureRect] for the icon.
+var texture_rect_icon: TextureRect
+
+
+func _init() -> void:
+	item_rect_changed.connect(_resize_children)
+	tree_entered.connect(_resize_children)
+	# texture_rect_icon ########################################################
+	texture_rect_icon = TextureRect.new()
+	add_child(texture_rect_icon)
+	texture_rect_icon.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	texture_rect_icon.stretch_mode = TextureRect.STRETCH_KEEP_ASPECT_COVERED
+	texture_rect_icon.texture = icon
+	texture_rect_icon.flip_h = flip_h
+	texture_rect_icon.flip_v = flip_v
+
+
+# Signal callables #############################################################
+
+
+func _resize_children() -> void:
+	texture_rect_icon.size = size
--- addons/widgets/scripts/w_nav_bar.gd
+++ addons/widgets/scripts/w_nav_bar.gd
@@ -1,103 +1,139 @@
 @tool
 class_name WNavBar
 extends PanelContainer
-## A widget for intended to be used for navigation arround the app screens/pages.
-## It might have zero height if [param custom_minimum_height] not specified.
+## A widget with to [WIconButton] and a [Label] in the middle. Intended to be
+## placed at the top/bottom of the scene (left/right buttons), but it can also
+## be placed on sides.
 
 
-## Emitted when left button is pressed.
+## Emitted when the left button is pressed.
 signal pressed_left
 ## Emitted when the right button is pressed.
 signal pressed_right
 
-
 @export_group("Title")
-## NavBar title.
+## [WNavBar] title.
 @export var title: String:
 	set(title_):
 		title = title_
-		if title_label != null:
-			title_label.text = title
+		if label_title != null:
+			label_title.text = title
 @export_enum(
 	"Horizontal alignment left",
 	"Horizontal alignment center",
 	"Horizontal alignment right",
 	"Horizontal alignment fill"
 )
-## NavBar title horizontal alignment.
-var horizontal_alignment: int = HORIZONTAL_ALIGNMENT_CENTER:
+## [WNavBar] title horizontal alignment.
+var horizontal_alignment: int:
 	set(horizontal_alignment_):
 		horizontal_alignment = horizontal_alignment_
-		if title_label != null:
-			title_label.horizontal_alignment = horizontal_alignment
+		if label_title != null:
+			label_title.horizontal_alignment = horizontal_alignment
 @export_group("Left button", "button_left")
-## Left button disabled state.
-@export var button_left_disabled: bool = false:
-	set(button_left_disabled_):
-		button_left_disabled = button_left_disabled_
-		if button_left != null:
-			button_left.disabled = button_left_disabled
-## Left button icon.
+## Left icon button [member WIconButton.icon].
 @export var button_left_icon: Texture2D:
 	set(button_left_icon_):
 		button_left_icon = button_left_icon_
 		if button_left != null:
 			button_left.icon = button_left_icon
+## Left icon button [member WIconButton.flip_h].
+@export var button_left_flip_h: bool:
+	set(button_left_flip_h_):
+		button_left_flip_h = button_left_flip_h_
+		if button_left != null:
+			button_left.flip_h = button_left_flip_h
+## Left icon button [member WIconButton.flip_v].
+@export var button_left_flip_v: bool:
+	set(button_left_flip_v_):
+		button_left_flip_v = button_left_flip_v_
+		if button_left != null:
+			button_left.flip_v = button_left_flip_v
+## Left icon button [member WIconButton.disabled].
+@export var button_left_disabled: bool:
+	set(button_left_disabled_):
+		button_left_disabled = button_left_disabled_
+		if button_left != null:
+			button_left.disabled = button_left_disabled
 @export_group("Right button", "button_right")
-## Left button disabled state.
-@export var button_right_disabled: bool = false:
-	set(button_right_disabled_):
-		button_right_disabled = button_right_disabled_
-		if button_right != null:
-			button_right.disabled = button_right_disabled
-## Left button icon.
+## Right icon button [member WIconButton.icon].
 @export var button_right_icon: Texture2D:
 	set(button_right_icon_):
 		button_right_icon = button_right_icon_
 		if button_right != null:
 			button_right.icon = button_right_icon
+## Right icon button [member WIconButton.flip_h].
+@export var button_right_flip_h: bool:
+	set(button_right_flip_h_):
+		button_right_flip_h = button_right_flip_h_
+		if button_right != null:
+			button_right.flip_h = button_right_flip_h
+## Right icon button [member WIconButton.flip_v].
+@export var button_right_flip_v: bool:
+	set(button_right_flip_v_):
+		button_right_flip_v = button_right_flip_v_
+		if button_right != null:
+			button_right.flip_v = button_right_flip_v
+## Right icon button [member WIconButton.disabled].
+@export var button_right_disabled: bool = false:
+	set(button_right_disabled_):
+		button_right_disabled = button_right_disabled_
+		if button_right != null:
+			button_right.disabled = button_right_disabled
 
+## Container for all elements.
 var container: HBoxContainer
-var title_label: Label
+## [Label] holding the title.
+var label_title: Label
+## Left [WIconButton].
 var button_left: WIconButton
+## Right [WIconButton].
 var button_right: WIconButton
 
 
-func _enter_tree() -> void:
+func _init() -> void:
+	item_rect_changed.connect(_resize_children)
+	tree_entered.connect(_resize_children)
+	# Container ################################################################
 	container = HBoxContainer.new()
 	add_child(container)
-	
+	# button_left ##############################################################
 	button_left = WIconButton.new()
 	container.add_child(button_left)
-	button_left.disabled = button_left_disabled
+	button_left.pressed.connect(_on_button_left_pressed)
 	button_left.icon = button_left_icon
-	
-	title_label = Label.new()
-	container.add_child(title_label)
-	title_label.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	title_label.horizontal_alignment = horizontal_alignment
-	title_label.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
-	title_label.text = title
-	
+	button_left.flip_h = button_left_flip_h
+	button_left.flip_v = button_left_flip_v
+	button_left.disabled = button_left_disabled
+	# label_title ##############################################################
+	label_title = Label.new()
+	container.add_child(label_title)
+	label_title.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	label_title.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	label_title.vertical_alignment = VERTICAL_ALIGNMENT_CENTER
+	label_title.horizontal_alignment = horizontal_alignment
+	label_title.text = title
+	# button_right #############################################################
 	button_right = WIconButton.new()
 	container.add_child(button_right)
-	button_right.disabled = button_right_disabled
+	button_right.pressed.connect(_on_button_right_pressed)
 	button_right.icon = button_right_icon
-	
-	button_left.pressed.connect(
-		func _on_button_left_pressed() -> void:
-			pressed_left.emit()
-	)
-	button_right.pressed.connect(
-		func _on_button_right_pressed() -> void:
-			pressed_right.emit()
-	)
+	button_right.flip_h = button_right_flip_h
+	button_right.flip_v = button_right_flip_v
+	button_right.disabled = button_right_disabled
 
 
-func _ready() -> void:
-	item_rect_changed.connect(
-	func _on_item_rect_changed() -> void:
+# Signal callables #############################################################
+
+
+func _resize_children() -> void:
 		button_left.custom_minimum_size = Vector2(size[1], 0)
 		button_right.custom_minimum_size = Vector2(size[1], 0)
-	)
-	item_rect_changed.emit()
+
+
+func _on_button_left_pressed() -> void:
+	pressed_left.emit()
+
+
+func _on_button_right_pressed() -> void:
+	pressed_right.emit()
